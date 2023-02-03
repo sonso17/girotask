f5ee8c4188acdb106d236979cef4ff009e6128d8
Aleix Poch <aleixpochvila@gmail.com>
2023-02-03 17:47:17 +0100

    entrar codigo

---
+++ src/components/capcaleraC.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="capcaleraGeneral">
+        <div id="capLogo"></div>
+        <div id="capNomP"></div>
+        <div id="capInfoUsuari">
+            <div id="capNomUsuari">{{ nomUsuari }}</div>
+            <div id="capRolUsuari">{{ rolUsuari }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#capcaleraGeneral{
+    position: absolute;
+    top: 0%;
+    height: 9%;
+    width: 100%;
+    /* top: 91%; */
+    left: 0%;
+    background-color: #FE7373;
+}
+#capLogo{
+    position: relative;
+    height: 97%;
+    width: 5%;
+    left: 1%;
+    top: -2%;
+    background-image: url('../assets/LogoDefinitiu.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+#capNomP{
+    
+}
+</style>
---
+++ src/components/peuPagina.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="peuPaginaGeneral">
+        <div id="peuPaginaC">© Copyright GiroTask</div>
+        <div id="peuPaginaC">Fet per Bernat Bigas i Aleix Poch</div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#peuPaginaGeneral{
+    font-size: 25px;
+    position: relative;
+    height: 28%;
+    width: 100%;
+    /* top: 91%; */
+    left: 0%;
+    background-color: #FE7373;
+}
+</style>
